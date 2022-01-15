582578e86f76ff2e35bd7548d5efe99f3bbd89a7
xl1870011370 <1605368195@qq.com>
2022-01-15 10:48:29 +0800

    update log dependency

--- java_process/process_linux.go
+++ java_process/process_linux.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"jrasp-daemon/defs"
-	"jrasp-daemon/ylog"
+	"jrasp-daemon/zlog"
 	"os"
 	"strings"
 	"time"
@@ -15,12 +15,12 @@ func IsLoaderJar(pid int32, jarName string) bool {
 	path := fmt.Sprintf("/proc/%d/maps", pid)
 	_, err := os.Stat(path)
 	if err != nil {
-		ylog.Warnf(defs.LOAD_JAR, "[isLoaderJar]", fmt.Sprintf("进程%d的maps文件不存在,err:%v", pid, err))
+		zlog.Warnf(defs.LOAD_JAR, "[isLoaderJar]", fmt.Sprintf("进程%d的maps文件不存在,err:%v", pid, err))
 		return false
 	}
 	buf, err := ioutil.ReadFile(path)
 	if err != nil {
-		ylog.Warnf(defs.LOAD_JAR, "[isLoaderJar]", fmt.Sprintf("打开进程%d的maps文件失败,err:%v", pid, err))
+		zlog.Warnf(defs.LOAD_JAR, "[isLoaderJar]", fmt.Sprintf("打开进程%d的maps文件失败,err:%v", pid, err))
 		return false
 	}
 	if strings.Contains(string(buf), jarName) {
