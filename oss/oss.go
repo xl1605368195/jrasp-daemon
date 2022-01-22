8b74f5090e2a033d0021e314e41a5e58750b6848
xl1870011370 <1605368195@qq.com>
2022-01-22 15:53:20 +0800

    名称修改

--- oss/oss.go
+++ oss/oss.go
@@ -43,8 +43,8 @@ func NewTxOssClient(cfg *userconfig.Config, env *environ.Environ) *TxOss {
 // DownLoad ossName，在对象的访问域名 `examplebucket-1250000000.cos.COS_REGION.myqcloud.com/test/objectPut.go` 中，
 // 对象键为 test/objectPut.go，ossName=test/objectPut.go
 // filePath，下载的绝对路径,例如 /opt/jrasp-agent/bin/jrasp-daemon_new
-func (this *TxOss) DownLoad(ossName, filePath string) error {
-	_, err := this.Client.Object.GetToFile(context.Background(), ossName, filePath, nil)
+func (oss *TxOss) DownLoad(ossName, filePath string) error {
+	_, err := oss.Client.Object.GetToFile(context.Background(), ossName, filePath, nil)
 	if err != nil {
 		return err
 	}
@@ -53,8 +53,8 @@ func (this *TxOss) DownLoad(ossName, filePath string) error {
 
 // UpLoad ossName，在对象的访问域名 `examplebucket-1250000000.cos.COS_REGION.myqcloud.com/test/objectPut.go` 中，对象键为 test/objectPut.go，ossName=test/objectPut.go
 // filePath，需要上传文件的绝对路径
-func (this *TxOss) UpLoad(ossName, filePath string) (bool, error) {
-	_, err := this.Client.Object.PutFromFile(context.Background(), ossName, filePath, nil)
+func (oss *TxOss) UpLoad(ossName, filePath string) (bool, error) {
+	_, err := oss.Client.Object.PutFromFile(context.Background(), ossName, filePath, nil)
 	if err != nil {
 		zlog.Errorf(defs.OSS_UPLOAD, "upload file failed", "ossName:%s,filePath:%s,err:%v", ossName, filePath, err)
 		return false, err
@@ -62,28 +62,28 @@ func (this *TxOss) UpLoad(ossName, filePath string) (bool, error) {
 	return true, nil
 }
 
-func (this *TxOss) UpdateDaemonFile() {
+func (oss *TxOss) UpdateDaemonFile() {
 	// 配置中可执行文件hash不为空，并且与env中可执行文件hash不相同
-	if this.cfg.ExeOssFileHash != "" && this.cfg.ExeOssFileHash != this.env.ExeFileHash {
-		newFilePath := filepath.Join(this.env.InstallDir, "bin/jrasp-daemon_new")
-		_ = this.DownLoad(this.cfg.ExeOssFileName, newFilePath)
+	if oss.cfg.ExeOssFileHash != "" && oss.cfg.ExeOssFileHash != oss.env.ExeFileHash {
+		newFilePath := filepath.Join(oss.env.InstallDir, "bin/jrasp-daemon_new")
+		_ = oss.DownLoad(oss.cfg.ExeOssFileName, newFilePath)
 		newHash, err := utils.GetFileHash(newFilePath)
 		if err != nil {
 			zlog.Errorf(defs.OSS_DOWNLOAD, "download  jrasp-daemon exec file", "err:%v", err)
 		} else {
-			this.checkHashAndReStart(newHash, newFilePath)
+			oss.checkHashAndReStart(newHash, newFilePath)
 		}
 	} else {
-		zlog.Infof(defs.OSS_DOWNLOAD, "no need to update jrasp-daemon", "userconfig.ExecOssFileHash:%s,env.ExecDiskFileHash:%s", this.cfg.ExeOssFileHash, this.env.ExeFileHash)
+		zlog.Infof(defs.OSS_DOWNLOAD, "no need to update jrasp-daemon", "userconfig.ExecOssFileHash:%s,env.ExecDiskFileHash:%s", oss.cfg.ExeOssFileHash, oss.env.ExeFileHash)
 	}
 }
 
-func (this *TxOss) checkHashAndReStart(newFileHash string, newFilePath string) {
+func (oss *TxOss) checkHashAndReStart(newFileHash string, newFilePath string) {
 	// 校验下载文件的hash
-	if newFileHash == this.cfg.ExeOssFileHash {
-		this.replace(newFileHash, newFilePath)
+	if newFileHash == oss.cfg.ExeOssFileHash {
+		oss.replace(newFileHash, newFilePath)
 	} else {
-		zlog.Errorf(defs.OSS_DOWNLOAD, "[BUG]check file hash err", "newFileHash:%s,configHash:%s", newFileHash, this.cfg.ExeOssFileHash)
+		zlog.Errorf(defs.OSS_DOWNLOAD, "[BUG]check file hash err", "newFileHash:%s,configHash:%s", newFileHash, oss.cfg.ExeOssFileHash)
 		err := os.Remove(newFilePath)
 		if err != nil {
 			zlog.Errorf(defs.OSS_DOWNLOAD, "[BUG]delete broken file err", "newFileHash:%s,fileHash:%s", newFilePath, newFileHash)
@@ -92,14 +92,14 @@ func (this *TxOss) checkHashAndReStart(newFileHash string, newFilePath string) {
 }
 
 // replace
-func (this *TxOss) replace(newFileHash string, newFilePath string) {
+func (oss *TxOss) replace(newFileHash string, newFilePath string) {
 	zlog.Infof(defs.OSS_DOWNLOAD, "check hash success", "hash:%s", newFileHash)
 	// 增加可执行权限
 	err := os.Chmod(newFilePath, 0700)
 	if err != nil {
 		zlog.Infof(defs.OSS_DOWNLOAD, "chmod +x jrasp-demon_new", "err:%v", err)
 	}
-	oldFilePath := filepath.Join(this.env.InstallDir, "bin/jrasp-daemon")
+	oldFilePath := filepath.Join(oss.env.InstallDir, "bin/jrasp-daemon")
 	err = os.Rename(oldFilePath, newFilePath)
 	if err == nil {
 		zlog.Infof(defs.OSS_DOWNLOAD, "update jrasp-daemon file success", "jrasp-daemon process will exit...")
@@ -111,25 +111,25 @@ func (this *TxOss) replace(newFileHash string, newFilePath string) {
 }
 
 // DownLoadModuleFiles 模块升级
-func (this *TxOss) DownLoadModuleFiles() {
-	files, err := ioutil.ReadDir(filepath.Join(this.env.InstallDir, "required-module"))
+func (oss *TxOss) DownLoadModuleFiles() {
+	files, err := ioutil.ReadDir(filepath.Join(oss.env.InstallDir, "required-module"))
 	if err != nil {
 		zlog.Errorf(defs.OSS_DOWNLOAD, "list disk module file failed", "err:%v", err)
 		return
 	}
 	// 1.先检测磁盘上的全部插件的名称、hash
-	fileHashMap := this.listModuleFile(files)
+	fileHashMap := oss.listModuleFile(files)
 	// 2.下载
-	this.downLoad(fileHashMap)
+	oss.downLoad(fileHashMap)
 }
 
-func (this *TxOss) downLoad(fileHashMap map[string]string) {
-	for _, m := range this.cfg.ModuleList {
+func (oss *TxOss) downLoad(fileHashMap map[string]string) {
+	for _, m := range oss.cfg.ModuleList {
 		hash, ok := fileHashMap[m.ModuleName]
 		if !ok || hash != m.Md5 {
 			// 下载
-			tmpFileName := filepath.Join(this.env.InstallDir, "required-module", m.ModuleName+".tmp")
-			err := this.DownLoad(m.DownLoadURL, tmpFileName) // module.jar.tmp
+			tmpFileName := filepath.Join(oss.env.InstallDir, "required-module", m.ModuleName+".tmp")
+			err := oss.DownLoad(m.DownLoadURL, tmpFileName) // module.jar.tmp
 			if err != nil {
 				zlog.Errorf(defs.OSS_DOWNLOAD, "[BUG]download file failed", "tmpFileName:%s,err:%v", tmpFileName, err)
 				continue
@@ -144,19 +144,19 @@ func (this *TxOss) downLoad(fileHashMap map[string]string) {
 			// 校验成功，修改名称
 			if diskFileHash == m.Md5 {
 				zlog.Infof(defs.OSS_DOWNLOAD, "check file hash success", "filePath:%s,hash:%v", tmpFileName, diskFileHash)
-				newFilePath := filepath.Join(this.env.InstallDir, "required-module", m.ModuleName)
+				newFilePath := filepath.Join(oss.env.InstallDir, "required-module", m.ModuleName)
 				_ = os.Rename(tmpFileName, newFilePath)
 			}
 		}
 	}
 }
 
-func (this *TxOss) listModuleFile(files []fs.FileInfo) map[string]string {
+func (oss *TxOss) listModuleFile(files []fs.FileInfo) map[string]string {
 	var fileHashMap = make(map[string]string)
 	for _, file := range files {
 		name := file.Name()
 		if !file.IsDir() && strings.HasSuffix(name, ".jar") {
-			hash, err := utils.GetFileHash(filepath.Join(this.env.InstallDir, "required-module", name))
+			hash, err := utils.GetFileHash(filepath.Join(oss.env.InstallDir, "required-module", name))
 			if err != nil {
 				zlog.Errorf(defs.OSS_DOWNLOAD, "[Fix it] calc file hash error", "file:%s,err:%v", name, err)
 			} else {
