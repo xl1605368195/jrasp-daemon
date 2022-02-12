353f98899c4d9f394c03c5b1a293df6d1c0ddb70
-1day <3379307903@qq.com>
2022-02-12 12:49:00 +0800

    add const explain

--- defs/defs.go
+++ defs/defs.go
@@ -21,14 +21,14 @@ const (
 	LOAD_JAR              int = START_LOG_ID + 8
 	UTILS_OpenFiles       int = START_LOG_ID + 9
 	WATCH_DEFAULT         int = START_LOG_ID + 10
-	HEART_BEAT            int = START_LOG_ID + 11
+	HEART_BEAT            int = START_LOG_ID + 11  // 心跳
 	NACOS_INIT            int = START_LOG_ID + 12
 	NACOS_LISTEN_CONFIG   int = START_LOG_ID + 13
 	OSS_DOWNLOAD          int = START_LOG_ID + 14
 	OSS_UPLOAD            int = START_LOG_ID + 15
 	DEPENDENCY_INFO       int = START_LOG_ID + 16
-	AGENT_SUCCESS_EXIT    int = START_LOG_ID + 17
-	JAVA_PROCESS_STARTUP  int = START_LOG_ID + 18
-	JAVA_PROCESS_SHUTDOWN int = START_LOG_ID + 19
-	AGENT_SUCCESS_INIT    int = START_LOG_ID + 20
+	AGENT_SUCCESS_EXIT    int = START_LOG_ID + 17  // agent 卸载成功
+	JAVA_PROCESS_STARTUP  int = START_LOG_ID + 18  // 发现java启动
+	JAVA_PROCESS_SHUTDOWN int = START_LOG_ID + 19  // 发现java退出
+	AGENT_SUCCESS_INIT    int = START_LOG_ID + 20  // agent 加载成功(attach成功)
 )
